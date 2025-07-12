e1a9eda510bbcb7994422672b01e89ec85b53027
ManikandaPrabhu-dev <manikandaprabhu3002@gmail.com>
2025-07-12 12:49:33 +0530

    Fix profile picture

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section id="hero" className="hero">
       <div className="hero-content">
-        <img className='hero-img' src="./src/assets/doc/myimage.jpg" alt="" />
+        <img className='hero-img' src="./public/myimage.jpg" alt="MY IMAGE" />
         <h1>Hello, I'm Manikandaprabhu</h1>
         <p>Recent IT graduate passionate about building responsive and dynamic web applications.</p>
         <a href="#contact" className="hero-button">Let's Connect</a>
